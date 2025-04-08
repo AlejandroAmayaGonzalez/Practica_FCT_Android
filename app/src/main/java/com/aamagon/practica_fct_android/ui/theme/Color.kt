e8eff76b04c1c7bfbd13a24a2262e5fa48e50de2
Alejandro <aamagon2004@gmail.com>
2025-04-08 14:31:04 +0200

    Cambio color toolbar y Home terminado

--- app/src/main/java/com/aamagon/practica_fct_android/ui/theme/Color.kt
+++ app/src/main/java/com/aamagon/practica_fct_android/ui/theme/Color.kt
@@ -11,4 +11,4 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 // Toolbar background
-val ToolbarBackground = Color(0xFF2196F3)
+val ToolbarBackground = Color(0xFF938E8E)
--- app/src/main/java/com/aamagon/practica_fct_android/ui/view/HomeScreen.kt
+++ app/src/main/java/com/aamagon/practica_fct_android/ui/view/HomeScreen.kt
@@ -1,14 +1,18 @@
 package com.aamagon.practica_fct_android.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.aamagon.practica_fct_android.R
 import com.aamagon.practica_fct_android.ui.view.toolbar.MainToolBar
 
 @Composable
@@ -24,10 +28,13 @@ fun HomeScreen(navController: NavController){
 
 @Composable
 fun HomeContent(modifier: Modifier = Modifier){
-    Column ( modifier = modifier.padding() ) {
-        Text(
-            text = "Inicio",
-            fontSize = 100.sp
+    Column (
+        modifier = modifier.padding().fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.home_image),
+            contentDescription = "Logo SmartSolar",
+            modifier = Modifier.height(300.dp).width(600.dp)
         )
     }
 }
