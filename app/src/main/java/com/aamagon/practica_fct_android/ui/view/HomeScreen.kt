82b71c6dcae027ba4aeb39bde20f702286f95738
Alejandro <aamagon2004@gmail.com>
2025-04-09 09:03:17 +0200

    Terminar home y correciones en toolbar y strings.xml

--- app/src/main/java/com/aamagon/practica_fct_android/ui/view/HomeScreen.kt
+++ app/src/main/java/com/aamagon/practica_fct_android/ui/view/HomeScreen.kt
@@ -1,14 +1,19 @@
 package com.aamagon.practica_fct_android.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.aamagon.practica_fct_android.R
 import com.aamagon.practica_fct_android.ui.view.toolbar.MainToolBar
 
 @Composable
@@ -25,9 +30,10 @@ fun HomeScreen(navController: NavController){
 @Composable
 fun HomeContent(modifier: Modifier = Modifier){
     Column ( modifier = modifier.padding() ) {
-        Text(
-            text = "Inicio",
-            fontSize = 100.sp
+        Image(
+            painter = painterResource(R.drawable.home_image),
+            contentDescription = stringResource(R.string.contentDescLogoSmart),
+            modifier = modifier.height(500.dp).width(500.dp)
         )
     }
 }
