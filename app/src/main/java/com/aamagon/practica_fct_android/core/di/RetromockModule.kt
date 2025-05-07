2dd4e95fc15dbca47fe623e615fa008d3cf55296
Alejandro <aamagon2004@gmail.com>
2025-05-07 11:39:00 +0200

    Implementación Retromock definitiva

--- app/src/main/java/com/aamagon/practica_fct_android/core/di/RetromockModule.kt
+++ app/src/main/java/com/aamagon/practica_fct_android/core/di/RetromockModule.kt
@@ -1,11 +1,14 @@
 package com.aamagon.practica_fct_android.core.di
 
+import android.content.Context
 import co.infinum.retromock.Retromock
+import com.aamagon.practica_fct_android.core.bodyFactory.AssetsBodyFactory
 import com.aamagon.practica_fct_android.data.network.bills.BillsApiClient
 import com.aamagon.practica_fct_android.data.network.detail.DetailApiClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -17,32 +20,40 @@ object RetromockModule {
     @BillService
     @Singleton
     @Provides
-    fun provideBillsRetromock(@BillService retrofit: Retrofit): Retromock{
+    fun provideBillsRetromock(
+        @BillService retrofit: Retrofit,
+        @ApplicationContext context: Context
+    ): Retromock{
         return Retromock.Builder()
             .retrofit(retrofit)
+            .defaultBodyFactory(AssetsBodyFactory(context.assets::open))
             .build()
     }
 
     @DetailService
     @Singleton
     @Provides
-    fun provideDetailRetromock(@DetailService retrofit: Retrofit): Retromock{
+    fun provideDetailRetromock(
+        @DetailService retrofit: Retrofit,
+        @ApplicationContext context: Context
+    ): Retromock{
         return Retromock.Builder()
             .retrofit(retrofit)
+            .defaultBodyFactory(AssetsBodyFactory(context.assets::open))
             .build()
     }
 
     @BillService
     @Singleton
     @Provides
-    fun provideBillsApiClient(@BillService retrofit: Retrofit): BillsApiClient{
-        return retrofit.create(BillsApiClient::class.java)
+    fun provideBillsApiClient(@BillService retromock: Retromock): BillsApiClient{
+        return retromock.create(BillsApiClient::class.java)
     }
 
     @DetailService
     @Singleton
     @Provides
-    fun provideDetailApiClient(@DetailService retrofit: Retrofit): DetailApiClient{
-        return retrofit.create(DetailApiClient::class.java)
+    fun provideDetailApiClient(@DetailService retromock: Retromock): DetailApiClient{
+        return retromock.create(DetailApiClient::class.java)
     }
 }
--- app/src/main/java/com/aamagon/practica_fct_android/data/network/detail/DetailApiClient.kt
+++ app/src/main/java/com/aamagon/practica_fct_android/data/network/detail/DetailApiClient.kt
@@ -1,6 +1,7 @@
 package com.aamagon.practica_fct_android.data.network.detail
 
 import co.infinum.retromock.meta.Mock
+import co.infinum.retromock.meta.MockResponse
 import com.aamagon.practica_fct_android.data.model.DetailModel
 import retrofit2.Response
 import retrofit2.http.GET
@@ -8,5 +9,6 @@ import retrofit2.http.GET
 interface DetailApiClient {
     @Mock
     @GET(".")
+    @MockResponse(body = "detalle.json")
     suspend fun getDetail(): Response<DetailModel>
 }
