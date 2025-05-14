457354672748041ca069c08f0120a7fe36f58947
Alejandro <aamagon2004@gmail.com>
2025-05-14 11:45:00 +0200

    Fix: Quitar toasts

--- app/src/main/java/com/aamagon/practica_fct_android/ui/view/screens/BillsScreen.kt
+++ app/src/main/java/com/aamagon/practica_fct_android/ui/view/screens/BillsScreen.kt
@@ -1,7 +1,6 @@
 package com.aamagon.practica_fct_android.ui.view.screens
 
 import android.annotation.SuppressLint
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -25,7 +24,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -71,10 +69,6 @@ fun BillsList(billsViewModel: BillsViewModel, scafPad: PaddingValues ){
 
     val billList = billsViewModel.billsList.observeAsState(emptyList())
 
-    // To show Toasts Errors
-    val context = LocalContext.current
-    val errMsg = stringResource(R.string.toastErr)
-
     LazyColumn (
         modifier = Modifier.fillMaxSize()
             .padding(scafPad)
@@ -85,10 +79,6 @@ fun BillsList(billsViewModel: BillsViewModel, scafPad: PaddingValues ){
                 bill = bill
             )
             Spacer( modifier = Modifier.height(8.dp) )
-        }
-
-        if (billList.value.isEmpty()){
-            Toast.makeText(context, errMsg, Toast.LENGTH_SHORT).show()
         }
     }
 }
